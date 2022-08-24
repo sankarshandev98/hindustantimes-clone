74aa75d66ddef51eef342eee0e79a3b8675cc609
sankarshandev98 <gsankar164@gmail.com>
2022-08-24 22:53:54 +0530

    update

---
+++ hindustan_times/src/Navbar_Component/n1.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./N1.css"
+export default function N1() {
+    return (
+        <div id="m">
+            <h1 id="heading">hello</h1>
+            <img src="https://www.hindustantimes.com/res/images/ht-logo.svg" alt="" />
+        </div>
+    )
+}
---
+++ hindustan_times/src/Navbar_Component/navbar.jsx
@@ -0,0 +1,8 @@
+import N1 from "./N1";
+import React from "react";
+
+export default function Navbar() {
+    return (
+        <N1 />
+    )
+}
