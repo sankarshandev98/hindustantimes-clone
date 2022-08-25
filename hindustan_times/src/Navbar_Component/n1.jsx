4932839f632cf95f0400c1cd22697a5b48d0c8e2
sankarshandev98 <gsankar164@gmail.com>
2022-08-25 17:05:57 +0530

    update

--- hindustan_times/src/Navbar_Component/n1.jsx
+++ hindustan_times/src/Navbar_Component/n1.jsx
@@ -7,12 +7,33 @@ export default function N1() {
     return (
         <div className="n1main">
             <div className="left">
-                
+                <div className="l1">
+                    <div className="l1left">
+                        <img src="https://cdn-user-icons.flaticon.com/23174/23174581/1661411802902.svg?token=exp=1661412704~hmac=35f1debfb7437ebdb78386c6cb62c66e" alt="" /><span>Explore</span>
+                    </div>
+                    <div className="l1right">
+                        <img src="https://cdn-icons-png.flaticon.com/512/3128/3128287.png" alt="" /><span>Search</span>
+                    </div>
+                </div>
+                <div className="l2">
+                    <h1>dgdbg</h1>
+                </div>
             </div>
             <div className="center"></div>
-            <div className="right"></div>
+            <div className="right">
+                <div className="r1">
+                    <div className="r1left">
+                        <img src="https://cdn-icons-png.flaticon.com/512/2331/2331852.png" alt="" /><span>Games</span>
+                    </div>
+                    <div className="r1center">
+                        <img src="https://cdn-icons-png.flaticon.com/512/3449/3449593.png" alt="" /><span>E-Paper</span>
+                    </div>
+                    <div className="r1right">
+                        <img src="https://cdn-icons-png.flaticon.com/512/3596/3596175.png" alt="" /><span>Sign in</span>
+                    </div>
+                </div>
+                <div className="r2"></div>
+            </div>
         </div>
     )
 }
-
-{/* <img src="https://www.hindustantimes.com/res/images/ht-logo.svg" alt="" /> */ }
